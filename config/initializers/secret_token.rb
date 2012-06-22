7d1b0650841aa192cc131119671c0293efa9ea4c
Michio Nakagawa <michio@ubuntu.localdomain>
2012-06-22 15:39:22 +0900

    新規作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRails::Application.config.secret_token = 'f154c37e34962f7eb7dbe4c15f7dd23a2727b6a431732c0a72b4438133ee234b0467770860e6c482320202c46e1af36a30907e44b9e2a6068de1950f2ef822a5'
